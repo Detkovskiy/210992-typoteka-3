a025e1afbe49a5429da3291de2b65d4ec20831c0
Yura Detkovskiy <9119113142@mail.ru>
2020-02-06 13:28:52 +0300

    файлы

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -11,69 +11,37 @@ const {
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
-const TITLES = [
-  'Ёлки. История деревьев',
-  'Как перестать беспокоиться и начать жить',
-  'Как достигнуть успеха не вставая с кресла',
-  'Обзор новейшего смартфона',
-  'Лучше рок-музыканты 20-века',
-  'Как начать программировать',
-  'Учим HTML и CSS',
-  'Что такое золотое сечение',
-  'Как собрать камни бесконечности',
-  'Борьба с прокрастинацией',
-  'Рок — это протест',
-  'Самый лучший музыкальный альбом этого года'
-];
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const FULLTEXT = [
-  'Ёлки — это не просто красивое дерево. Это прочная древесина.',
-  'Первая большая ёлка была установлена только в 1938 году.',
-  'Вы можете достичь всего. Стоит только немного постараться и запастись книгами.',
-  'Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.',
-  'Золотое сечение — соотношение двух величин, гармоническая пропорция.',
-  'Собрать камни бесконечности легко, если вы прирожденный герой.',
-  'Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.',
-  'Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.',
-  'Программировать не настолько сложно, как об этом говорят.',
-  'Простые ежедневные упражнения помогут достичь успеха.',
-  'Это один из лучших рок-музыкантов.',
-  'Он написал больше 30 хитов.',
-  'Из под его пера вышло 8 платиновых альбомов.',
-  'Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.',
-  'Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?',
-  'Достичь успеха помогут ежедневные повторения.',
-  'Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.',
-  'Как начать действовать? Для начала просто соберитесь.',
-  'Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.',
-  'Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.'
-];
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
-const CATEGORIES = [
-  'Деревья',
-  'За жизнь',
-  'Без рамки',
-  'Разное',
-  'IT',
-  'Музыка',
-  'Кино',
-  'Программирование',
-  'Железо'
-];
-
-const generateOffers = (count) => (
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    announce: shuffle(FULLTEXT).slice(1, 5).join(` `),
-    fullText: shuffle(FULLTEXT).slice(1, getRandomInt(0, FULLTEXT.length - 1)).join(` `),
+    title: titles[getRandomInt(0, titles.length - 2)],
+    announce: shuffle(sentences).slice(1, 5).join(` `),
+    fullText: shuffle(sentences).slice(1, getRandomInt(0, sentences.length - 2)).join(` `),
     createdDate: moment().subtract(getRandomInt(1, 30), 'days').subtract(getRandomInt(0, 2), 'months').format('YYYY-MM-DD hh:mm:ss'),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, 4)),
+    category: shuffle(categories).slice(0, getRandomInt(1, 4)),
   }))
 );
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
 
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
@@ -82,7 +50,7 @@ module.exports = {
       return console.error(chalk.red(`Не больше 1000 объявлений!`));
     }
 
-    const content = JSON.stringify(generateOffers(countOffer), null, 2);
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences), null, 2);
 
     try {
       await fs.writeFile(FILE_NAME, content);
